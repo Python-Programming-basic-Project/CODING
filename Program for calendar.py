71971a6fe17d61b6e5cc1b1724cc6f9e7c018ed2
Sunny Gupta <76914430+SunnyGupta01@users.noreply.github.com>
2021-02-04 15:28:58 +0530

    Add files via upload

---
+++ Program for calendar.py
@@ -0,0 +1,13 @@
+# Program for calendar
+
+import calendar
+
+yy = 2014
+mm = 11
+
+
+yy = int(input("Enter year: "))
+mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
